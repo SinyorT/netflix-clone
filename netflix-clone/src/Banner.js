8b39419255c0c6034f9900d0d135fde83d7f9f63
Sinyor <abdurahmanov.togrul@gmail.com>
2021-05-02 18:16:32 +0300

    themoviedb API implementation

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,31 +1,50 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './Banner.css'
+import axios from './axios'
+import requests from './Requests'
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
 
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals) 
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            )
+            return request;
+        }
+        fetchData();
+    }, [])
+console.log(movie)
     function truncate(string, n) {
         return string?.length > n ? string.substring(0, n - 1) + '...' : string;
     }
+
 
     return (
         <header className="banner"
             style={{
                 backgroundSize: 'cover',
-                backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg')`,
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${
+                    movie?.backdrop_path
+                }")`,
                 backgroundPosition: 'center center'
             }}
         >
             <div className="banner__contents">
-                <h1 className="banner__title">Movie Name</h1>
+                <h1 className="banner__title">
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
 
                 <h1 className="banner__description">
-                    {truncate(`
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-                    `, 150)}
+                    {truncate(movie?.overview, 150)}
                 </h1>
             </div>
             <div className="banner--fadeBottom" />
