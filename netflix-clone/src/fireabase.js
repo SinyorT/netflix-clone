469cba23b16e6daecc6084e7c33c3809b9537509
Sinyor <abdurahmanov.togrul@gmail.com>
2021-05-02 14:33:21 +0300

    create project environment

---
+++ netflix-clone/src/fireabase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC2KmDZnI32morkE1yRiFtNS7mJOvyRXAc",
+    authDomain: "netflix-clone-7f180.firebaseapp.com",
+    projectId: "netflix-clone-7f180",
+    storageBucket: "netflix-clone-7f180.appspot.com",
+    messagingSenderId: "424299371758",
+    appId: "1:424299371758:web:fbae90a3ecdba26b4d27ff"
+  };
